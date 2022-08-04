e14a008a1a8bb7e86137acb57b76dbe3acba6899
Om Vyas <ovyas24@gmail.com>
2022-08-04 23:54:55 +0530

    added functional variability to mdb connection

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,5 +1,10 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api');
-
-
+module.exports = () => {
+  mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api');
+  const db = mongoose.connection;
+  db.on("error", console.error.bind(console, "connection error: "));
+  db.once("open", function () {
+    console.log("Connected successfully");
+  }); 
+}
